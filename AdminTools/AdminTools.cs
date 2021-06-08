bc90a701e91eb9984104d4d40a0e95cffa2ab39c
Feli <felinegri5@gmail.com>
2021-06-07 23:51:11 -0300

    Registrar permiso

--- AdminTools/AdminTools.cs
+++ AdminTools/AdminTools.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
+using OpenMod.API.Permissions;
 using OpenMod.API.Plugins;
 using OpenMod.Unturned.Plugins;
 using SDG.Unturned;
@@ -14,18 +15,27 @@ namespace AdminTools
     public class AdminTools : OpenModUnturnedPlugin
     {
         private readonly ILogger<AdminTools> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IPermissionRegistry _permissionregistry;
 
         public AdminTools(
             ILogger<AdminTools> logger,
+            IConfiguration configuration,
+            IPermissionRegistry permissionRegistry,
             IServiceProvider serviceProvider) : base(serviceProvider)
         {
             _logger = logger;
+            _configuration = configuration;
+            _permissionregistry = permissionRegistry;
         }
 
         protected override UniTask OnLoadAsync()
         {
             _logger.LogInformation("Admin Tools plugin made by Feli");
             _logger.LogInformation("Discord: https://discord.gg/4FF2548");
+
+            _permissionregistry.RegisterPermission(this, _configuration["FlyService:FlyPermission"], "A permission to be able to fly");
+
             return UniTask.CompletedTask;
         }
 
